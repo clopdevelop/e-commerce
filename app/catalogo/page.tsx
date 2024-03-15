ce0972aea22508a6105e0411f6b1d810705469b6
clopdevelop <yakiiloop@gmail.com>
2024-03-15 23:51:31 +0100

    Añadiendo estructura básuca de páginas

---
+++ app/catalogo/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='py-10'>Tienda</h1>
+    </>
+  );
+}
---
+++ app/politicas/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>Página políticas de la Tienda</h1>
+    </>
+  );
+}
