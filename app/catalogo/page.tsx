3b627fd0fc4dfb1f03f8a59ff788d1b5cc6147bc
clopdevelop <yakiiloop@gmail.com>
2024-03-21 11:46:15 +0100

    Añdiendo Componentes, Buscador, Productos, Paginación

--- app/catalogo/page.tsx
+++ app/catalogo/page.tsx
@@ -1,34 +1,31 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import ProductsTable from "@/components/ProductsTable";
+import Search from "@/components/Search";
+import MyPagination from "@/components/myPagination";
 
-import { data } from "@/lib/data";
+import { fetchProductsPages } from '@/lib/data';
 
-export default async function Home() {
-  const products = await data;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchProductsPages(query);
   return (
     <>
       <h1 className="py-10">Tienda</h1>
-      <div className="flex flex-row">
-        {products.map((product) => (
-          <Card className="m-4 max-w-80 min-w-80 max-h-96 min-h-96" key={product.id_product}>
-            <CardHeader>
-              <CardTitle className="h-14 leading-relaxed line-clamp-2 text-balance">{product.name}</CardTitle>
-              <CardDescription className="h-16">{product.description} con sillon y mesa del ikea y sus muertos a caballo punto com y eso to lo que te quieran vender</CardDescription>
-            </CardHeader>
-            <CardContent className="h-40">
-            Imagen {/* TODO Añadir imagenes */}
-            </CardContent>
-            <CardFooter>
-              <p>{product.price}€</p>
-            </CardFooter>
-          </Card>
-        ))}
+      <div className="my-5 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Buscar productos..." />
+      </div>
+      <ProductsTable 
+       query={query} currentPage={currentPage} />
+      <div className="mt-5 flex w-full justify-center">
+          <MyPagination totalPages={totalPages} currentPage={currentPage}></MyPagination>
       </div>
     </>
   );
