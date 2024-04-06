ced7d358dc090d378837d9bb64e76b2342f25b8a
clopdevelop <yakiiloop@gmail.com>
2024-04-06 15:25:45 +0200

    cambios estructura componentes

--- app/catalogo/page.tsx
+++ app/catalogo/page.tsx
@@ -1,8 +1,8 @@
-import ProductsTable from "@/components/ProductsTable";
-import Search from "@/components/Search";
-import MyPagination from "@/components/myPagination";
+import ProductsTable from "@/components/products/ProductsTable";
+import Search from "@/components/utils/Search";
+import MyPagination from "@/components/utils/myPagination";
 
-import { fetchFilteredProducts } from "@/lib/data";
+import { fetchProducts } from "@/lib/data";
 import { fetchProductsPages } from '@/lib/data';
 import { auth, getUser } from "@/auth";
 
@@ -17,7 +17,7 @@ export default async function Home({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
-  const filteredProducts = await fetchFilteredProducts(query, currentPage);
+  const Products = await fetchProducts(currentPage);
 
 
   const totalPages = await fetchProductsPages(query);
@@ -36,7 +36,7 @@ export default async function Home({
         <Search placeholder="Buscar productos..." />
       </div>
       <ProductsTable 
-       filteredProducts={filteredProducts} id_user={id_user} />
+       Products={Products} id_user={id_user} />
       <div className="mt-5 flex w-full justify-center">
           <MyPagination totalPages={totalPages} currentPage={currentPage}></MyPagination>
       </div>
