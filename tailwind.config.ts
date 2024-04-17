f9936245a7ebf21dafa0ea106eed5da6c5608d9e
Cristian López Gómez <cristian.lopez.developer@gmail.com>
2024-04-17 14:15:32 +0200

    añadiendo tablas al modelo de prisma para la autenticacion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,7 @@ import { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
